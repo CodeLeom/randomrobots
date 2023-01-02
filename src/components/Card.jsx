1ae13ba7ff4c2f61a7f65c0042ceed3281de7f07
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-01-02 03:15:50 +0100

    updated the card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card({item}) {
   return (
     <div className='tc bg-light-blue dib br3 pa3 ma3 grow bw2 shadow-2 '>
-        <img src={`https://robohash.org/${item.username}?200x200`} alt="random image of a robot" />
+        <img src={`https://robohash.org/${item.username}?200x200`} alt="random robot character" />
         <h2>{item.name}</h2>
         <p>{item.email}</p>
     </div>
